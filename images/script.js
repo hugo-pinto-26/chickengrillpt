c93065d87e2d36223747d1a459a9df92074fc9b4
hugo-pinto-26 <hugo.sousa-pinto@proton.me>
2024-12-18 14:11:41 +0100

    clean up

--- images/script.js
+++ images/script.js
@@ -513,8 +513,7 @@ jQuery(function($) {
         $("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
     }
     $(".newsletter-form").ajaxChimp({
-        url: "https://hibootstrap.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9",
+        url: "",
         callback: callbackFunction
     });
-    $('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
 }($));
